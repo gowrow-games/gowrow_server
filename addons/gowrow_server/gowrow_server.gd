935a63fe729868f477a928c2c3799ed3fe5dcf5a
James Coppock <james.coppock98@gmail.com>
2025-09-24 17:17:39 -0500

    fix: update plugin naming

---
+++ addons/gowrow_server/gowrow_server.gd
@@ -0,0 +1,13 @@
+@tool
+extends EditorPlugin
+
+
+func _enable_plugin() -> void:
+	print("Enabling Gowrow Server plugin...")
+	add_autoload_singleton("GowrowNetwork", "gowrow_network.gd")
+	add_autoload_singleton("GowrowLogger", "gowrow_logger.gd")
+
+func _disable_plugin() -> void:
+	print("Disabling Gowrow Server plugin...")
+	remove_autoload_singleton("GowrowNetwork")
+	remove_autoload_singleton("GowrowLogger")

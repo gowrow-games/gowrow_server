666b018393e538e3416421d30fde6a28d2a770da
James Coppock <james.coppock98@gmail.com>
2025-09-24 11:14:48 -0500

    refactor: remove empty multiplayer_spawner script

--- addons/gowrow_dedicated_server/examples/simple_2d_characters/multiplayer_spawner.gd
+++
@@ -1,9 +0,0 @@
-extends MultiplayerSpawner
-
-@export var character_scene: PackedScene
-
-# func _spawn(data: Variant) -> Node:
-# 	var character: Character = character_scene.instantiate()
-# 	character.set_multiplayer_authority(data["id"])
-# 	character.name = "Character_%d" % data["id"]
-# 	return character
